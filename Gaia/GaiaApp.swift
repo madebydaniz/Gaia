4690a35b93c1a39dc504721605a50764bf650460
Daniel Niazmand <madebydaniz@gmail.com>
2026-04-30 01:10:41 +0200

    Initial Commit

---
+++ Gaia/GaiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaiaApp.swift
+//  Gaia
+//
+//  Created by Daniel Niazmand on 30.04.26.
+//
+
+import SwiftUI
+
+@main
+struct GaiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GaiaTests/GaiaTests.swift
@@ -0,0 +1,19 @@
+//
+//  GaiaTests.swift
+//  GaiaTests
+//
+//  Created by Daniel Niazmand on 30.04.26.
+//
+
+import Testing
+@testable import Gaia
+
+struct GaiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
